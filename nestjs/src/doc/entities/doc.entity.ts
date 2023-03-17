d7fed4e0ecdc03abf744fd59564d2efc72ba5334
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-03-18 00:53:38 +0900

    [ADD] doc_module, doc controller, validation pipe

---
+++ nestjs/src/doc/entities/doc.entity.ts
@@ -0,0 +1,15 @@
+export class ListDocData {
+	document_id : number;
+	title : string;
+	thumbnail : string;
+	signature_count : number;
+}
+
+export class DetailDocData {
+	document_id : number;
+	title: string;
+	thumbnail : string;
+	signature_count: number;
+	description : string;
+	author_id : string;
+}
