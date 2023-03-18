52a959332d6b4ea09d8db8ded0f0d4aa0f90879e
parkjoungwan <whddhs6645@gmail.com>
2023-03-18 13:21:22 +0900

    [EDIT] NESTJS testModule 삭제

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -2,17 +2,17 @@ import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { LoggerMiddleware } from "./logger/logger.middleware";
-import { TestModule } from "./test/test.module";
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from "@nestjs/config";
 import { DocModule } from './doc/doc.module';
 
 @Module({
-  imports: [TestModule, AuthModule, ConfigModule.forRoot({ isGlobal: true }), DocModule],
+  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true }), DocModule],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
+
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes("*");
