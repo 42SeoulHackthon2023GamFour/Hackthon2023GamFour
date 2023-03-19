394111c1ade04d387cd1ff5f997ec128cc031fa6
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-03-19 20:29:10 +0900

    [FIX] fix storage limit

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
       transform: true,
     })
   );
+  app.use(express.json({limit: '50mb'}));
+  app.use(express.urlencoded({limit: '50mb'}));
   await app.listen(3000);
 }
 bootstrap();
